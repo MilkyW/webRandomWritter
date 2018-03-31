6aa8fcac2b014df6060ae0f84decb959912fe80e
Milky.W <minniewu@qq.com>
2018-03-31 12:08:38 +0800

    add log4j and err input check

--- src/main/java/com/randomwritter/demo/handler.java
+++ src/main/java/com/randomwritter/demo/handler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.apache.log4j.Logger;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -17,22 +18,46 @@ import java.io.PrintStream;
 public class handler {
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String say() {
-        return "Hello World";
+        return "try: http://localhost:8080/RandomWritter?filename=hamlet.txt&n=3&len=100";
     }
 
     @RequestMapping("/RandomWritter")
     public @ResponseBody
     String rw(
-            @RequestParam(name = "filename", required = false) String filename,
-            @RequestParam(name = "n", required = false) Integer n,
-            @RequestParam(name = "len", required = false) Integer len) {
+            @RequestParam(name = "filename", required = false, defaultValue = "") String filename,
+            @RequestParam(name = "n", required = false, defaultValue = "") String n,
+            @RequestParam(name = "len", required = false, defaultValue = "") String len) {
+        Logger LOGGER = Logger.getLogger(RandomWritter.class);
+        if (filename.equals(""))
+            return "Error: please enter filename!\t" +
+                    "e.g. filename = hamlet.txt\n";
+        if (n.equals(""))
+            return "Error: please enter n!\t" +
+                    "e.g. n = 3\n";
+        if (len.equals(""))
+            return "Error: please enter len!\t" +
+                    "e.g. len = 10\n";
         RandomWritter rm = new RandomWritter();
-        String in = filename + '\n' + String.valueOf(n) + '\n'
-                + String.valueOf(len) + '\n';
+        String in = filename + '\n' + n + '\n'
+                + len + '\n';
         ByteArrayInputStream is = new ByteArrayInputStream(in.getBytes());
         System.setIn(is);
         rm.out = "";
-        rm.main();
-        return rm.out+'\n';
+        try {
+            rm.main();
+        } catch (Exception e) {
+            if (!rm.err.equals(""))
+                return "Error: " + rm.err;
+            return e.getMessage();
+        }
+        if (rm.out.equals("")) {
+            if (!rm.err.equals(""))
+                return "Error: " + rm.err;
+            LOGGER.error("filename: " + filename + '\t'
+                    + "N: " + n + '\t'
+                    + "len: " + len + '\n');
+            return "Error: " + "unknown error!";
+        }
+        return rm.out + '\n';
     }
 }
